30cf4164f19ff79ef8d32e7ec059ab6218cb6926
Anthony Licata <tamamaroro@gmail.com>
2016-11-28 17:52:55 -0500

    Initial unity project

---
+++ RhythmTests/Assets/Scripts/BasicSequencer.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class BasicSequencer : MonoBehaviour {
+
+    public AudioSource bgmSource;
+    public AudioSource testSfxSource;
+
+    public AudioClip bgmClip = new AudioClip();
+    public AudioClip testSfxClip = new AudioClip();
+
+    Metronome metro;
+
+    public bool[] sequencer = new bool[16];
+    int seqPosition = 0;
+
+	// Use this for initialization
+	void Start () {
+        bgmSource = AddAudio(bgmClip, true, false, 1f);
+        testSfxSource = AddAudio(testSfxClip, false, false, 1f);
+        metro = gameObject.GetComponent<Metronome>();
+        metro.OnTick += Tick;
+        bgmSource.Play();
+	}
+
+    public AudioSource AddAudio(AudioClip clip, bool loop, bool playAwake, float vol) {
+        AudioSource audio = gameObject.AddComponent<AudioSource>();
+        audio.clip = clip;
+        audio.loop = loop;
+        audio.playOnAwake = playAwake;
+        audio.volume = vol;
+        return audio;
+    }
+
+    private void Tick(Metronome metronome)
+    {
+        if (sequencer[seqPosition])
+        {
+            testSfxSource.Play();
+        }
+        seqPosition++;
+        if (seqPosition >= sequencer.Length)
+        {
+            seqPosition = 0;
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
---
+++ RhythmTests/Assets/Scripts/Metronome.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Metronome.cs by Julien Noble, 2013
+// http://cubeslam.net/2013/12/19/unity-metronome-like-a-pro/
+
+public delegate void MetronomeEvent(Metronome metronome);
+
+public class Metronome : MonoBehaviour
+{
+    public int Base;
+    public int Step;
+    public float BPM;
+    public int CurrentStep = 1;
+    public int CurrentMeasure;
+
+    private float interval;
+    private float nextTime;
+
+    public event MetronomeEvent OnTick;
+    public event MetronomeEvent OnNewMeasure;
+
+    // Use this for initialization
+    void Start()
+    {
+        StartMetronome();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void StartMetronome()
+    {
+        StopCoroutine("DoTick");
+        CurrentStep = 1;
+        var multiplier = Base / 4f;
+        var tmpInterval = 60f / BPM;
+        interval = tmpInterval / multiplier;
+        nextTime = Time.time;
+        StartCoroutine("DoTick");
+    }
+
+    IEnumerator DoTick()
+    {
+        for (;;)
+        {
+            if (CurrentStep == 1 && OnNewMeasure != null)
+                OnNewMeasure(this);
+            if (OnTick != null)
+                OnTick(this);
+            nextTime += interval;
+            yield return new WaitForSeconds(nextTime - Time.time);
+            CurrentStep++;
+            if (CurrentStep > Step)
+            {
+                CurrentStep = 1;
+                CurrentMeasure++;
+            }
+        }
+    }
+}
